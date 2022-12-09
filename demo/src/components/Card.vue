f26bfa1762b3f31406004fe9f79d04a461bca2a9
Oscar Magnusson <oscar.c.magnusson@sr.se>
2022-12-09 13:22:20 +0100

    Fix cards

--- demo/src/components/Card.vue
+++ demo/src/components/Card.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 interface Dog {
   img: string;
@@ -8,16 +8,20 @@ interface Dog {
 }
 
 const dogs: Dog[] = [
-  { img: 'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg', summary: 'En fin hund.', main: 'En mycket fin hund faktiskt' },
   {
-    img: 'https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d',
-    summary: 'En ledsen hund.',
-    main: 'Ser ledsen ut.',
+    img: "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg",
+    summary: "En fin hund.",
+    main: "En mycket fin hund faktiskt",
   },
   {
-    img: 'https://www.thesprucepets.com/thmb/7TDhfkK5CAKBWEaJfez6607J48Y=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg',
-    summary: 'Pug',
-    main: 'Också gullig',
+    img: "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
+    summary: "En ledsen hund.",
+    main: "Ser ledsen ut.",
+  },
+  {
+    img: "https://www.thesprucepets.com/thmb/7TDhfkK5CAKBWEaJfez6607J48Y=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg",
+    summary: "Pug",
+    main: "Också gullig",
   },
 ];
 
@@ -40,8 +44,8 @@ const random = ref(Math.floor(Math.random() * dogs.length - 1) + 1);
   grid-template-rows: 1fr 1fr;
 
   grid-template-areas:
-    'img summary'
-    'main main';
+    "img summary"
+    "main main";
   background-color: #fff;
   color: #444;
   background: #fff;
@@ -53,7 +57,8 @@ const random = ref(Math.floor(Math.random() * dogs.length - 1) + 1);
   width: calc(100% - 24px);
   max-height: 300px;
 
-  container-type: inline-size;
+  /* example 3 */
+  // container-type: inline-size;
 }
 
 .summary {
@@ -73,23 +78,25 @@ const random = ref(Math.floor(Math.random() * dogs.length - 1) + 1);
   min-height: 150px;
 }
 
-@container (max-width: 275px) {
-  .summary {
-    font-size: 20px;
-  }
-}
+/* example 2 */
 
-@container (max-width: 250px) {
-  .wrapper {
-    display: flex;
-    grid-template-columns: 120px;
-    grid-template-rows: 120px 120px 120px;
-    grid-template-areas:
-      'img'
-      'summary'
-      'main';
-    flex-direction: column;
-    align-items: center;
-  }
-}
+// @container (max-width: 275px) {
+//   .summary {
+//     font-size: 20px;
+//   }
+// }
+
+// @container (max-width: 250px) {
+//   .wrapper {
+//     display: flex;
+//     grid-template-columns: 120px;
+//     grid-template-rows: 120px 120px 120px;
+//     grid-template-areas:
+//       "img"
+//       "summary"
+//       "main";
+//     flex-direction: column;
+//     align-items: center;
+//   }
+// }
 </style>
